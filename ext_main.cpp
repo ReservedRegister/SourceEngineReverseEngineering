94b7c51e07bc2bc480856a18dd317a0992ad3871
ReservedRegister <yohanes_zelezov@yahoo.co.uk>
2023-07-17 19:58:29 +0100

    order

--- ext_main.cpp
+++ ext_main.cpp
@@ -73,11 +73,11 @@ void ApplyPatches()
     offset = (uint32_t)Hooks::SimulateEntitiesHook - hook_game_frame_delete_list - 5;
     *(uint32_t*)(hook_game_frame_delete_list+1) = offset;
 
-    //uint32_t delete_list_call = server_srv + 0x00944F61;
-    //memset((void*)delete_list_call, 0x90, 5);
+    uint32_t delete_list_call = server_srv + 0x00944F61;
+    memset((void*)delete_list_call, 0x90, 5);
 
-    //uint32_t delete_list_call_sim = server_srv + 0x00A7AC57;
-    //memset((void*)delete_list_call_sim, 0x90, 5);
+    uint32_t delete_list_call_sim = server_srv + 0x00A7AC57;
+    memset((void*)delete_list_call_sim, 0x90, 5);
 
     //delete_list_call = server_srv + 0x00944FC5;
     //memset((void*)delete_list_call, 0x90, 5);
